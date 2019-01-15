efca9f356b6a2ffb5281fabd308acfe5e72305ac
Zhihang-Liu <enterindex@gmail.com>
2019-01-15 14:16:19 +0800

    名程序员lyzh，两小时解决lexer！（逃

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
-
 use std::io;
-use std::io::Write;
 
-use parser::lexer::lexer;
-use parser::parser;
-use runtime::context::ThreadContext;
-use runtime::tools::exception_to_string;
-
-pub mod value;
-pub mod func;
-pub mod parser;
-pub mod runtime;
-
+mod lexer;
 
 pub fn copyright() -> String {
     let os_info = if cfg!(target_os = "linux") {
@@ -67,54 +53,14 @@ pub fn copyright() -> String {
 "
 }
 
-fn main() {
-    println!("{}", copyright());
-    // 创建上下文对象
-    let ic = ThreadContext::new();
-    loop {
-        let _ = std::io::stdout().write("λ ".as_ref());
-        let _ = std::io::stdout().flush();
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).
-            unwrap();
-        //input.trim_right();
-        // lexer
-        match lexer(input.as_ref()) {
-            Ok(tf) =>
-            // parser
-                match parser(tf.as_ref()) {
-                    Ok(x) => {
-                        for i in x {
-                            /*
-                            let o = match i.compiler_eval(&ic) {
-                                Ok(o) => {
-                                    //println!("{} -> {}", o.get_type(), o.to_string());
-                                    o
-                                }
-                                Err(e) => {
-                                    match exception_to_string(e.clone()) {
-                                        Some(x) => eprintln!("{}", x),
-                                        None => eprintln!("exception {}", e.to_string())
-                                    }
-                                    Value::Nil
-                                }
-                            };
-                            */
 
-                            match i.eval(&ic) {
-                                Ok(o) => println!("{} -> {}", o.get_type(), o.to_string()),
-                                Err(e) => match exception_to_string(e.clone()) {
-                                    Some(x) => eprintln!("{}", x),
-                                    None => eprintln!("exception {}", e.to_string())
-                                },
-                            }
-                        }
-                    }
-                    Err((pos, info)) =>
-                        eprintln!("*** parser-error: {}, {}:{}", info, tf[pos].pos.line, tf[pos].pos.col),
-                }
-            Err(e) =>
-                eprintln!("*** lexer-error: 字符串未结束, {}:{}", e.line, e.col),
-        }
+fn main() {
+    //println!("{}", copyright());
+    loop {
+        println!(">>> ");
+        let mut src = String::new();
+        let _ = io::stdin().read_line(&mut src);
+        let token_stream = lexer::lexer(src.trim_right_matches("\r")).unwrap();
+        println!("{:?}", token_stream);
     }
 }
